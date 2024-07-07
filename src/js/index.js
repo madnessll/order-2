9e6c0da34f1840845507976e2494d9b030fd2c3d
Kot Bantik <supercat@vv.com>
2024-07-07 22:04:21 +0700

    слайдер адаптирован

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,30 @@
-// import Swiper bundle with all modules installed
-import Swiper from "swiper/bundle";
-
-import "swiper/css/bundle";
-
-const swiper = new Swiper(".financing__swiper", {
-  // Navigation arrows
-  navigation: {
-    nextEl: ".financing__button-next",
-    prevEl: ".financing__button-prev",
-  },
-
+$('.financing__swiper').slick({
+    rows: 2,
+    slidesPerRow: 3,
+    prevArrow: '.financing__button-prev',
+    nextArrow: '.financing__button-next',
+    infinite: false,
+    responsive: [
+        {
+            breakpoint: 885,
+            settings: {
+                slidesPerRow: 1,
+                rows: 1
+            }
+        },
+        {
+            breakpoint: 1312,
+            settings: {
+                slidesPerRow: 2,
+                rows: 1
+            }
+        },
+        {
+            breakpoint: 1400,
+            settings: {
+                slidesPerRow: 3,
+                rows: 2
+            }
+        }
+    ]
 });
