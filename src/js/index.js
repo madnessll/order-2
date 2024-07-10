125f87889af76b676f722999cac21dd543dadc98
Kot Bantik <supercat@vv.com>
2024-07-10 12:03:42 +0700

    сделал бургер

--- src/js/index.js
+++ src/js/index.js
@@ -33,3 +33,20 @@ document.querySelector('.burger').addEventListener('click',function(){
     this.classList.toggle('active');
     document.querySelector('.mobile-menu').classList.toggle('open');
 })
+// ///////////////////////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdowns = document.querySelectorAll('.mobile-menu__dropdown');
+
+  dropdowns.forEach(dropdown => {
+    const dropdownImg = dropdown.querySelector('.mobile-menu__dropdown-img');
+
+    dropdown.addEventListener('toggle', function () {
+      if (dropdown.open) {
+        dropdownImg.classList.add('rotated');
+      } else {
+        dropdownImg.classList.remove('rotated');
+      }
+    });
+  });
+});
+
